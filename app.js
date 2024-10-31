37960dcb21e3b2caeeb7e228a2cd89768ac0436b
Xander Glanninger <141350991+xanderglanninger@users.noreply.github.com>
2024-10-31 13:41:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+const port = process.env.PORT || 5000;
+
+
+const tempHtml = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send(tempHtml)
+})
+
+app.post('/data', (req, res) => {
+    const sensorData = req.body;
+    console.log('Received sensor data:', sensorData);
+    
+    // Here you can process the data (e.g., save to database)
+    
+    res.json({
+        status: 'success',
+        message: 'Data received successfully'
+    });
+res.send(sensorData);
+});
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
